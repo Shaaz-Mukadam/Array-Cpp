2bb075d55fb216f4b9fc13047988a67c9019ae47
Shaaz Mukadam <84740787+Shaaz-Mukadam@users.noreply.github.com>
2023-06-15 22:22:15 +0530

    Add files via upload

---
+++ Array1.cpp
@@ -0,0 +1,13 @@
+//**********ARRAY****************
+//Array is a data structures. Array is used to store the list of variable of similiar data types.
+//Array is used to store the similar type of data.
+//syntax: initialization:   dataType arrayName[size]   eg: int array[5]
+#include<iostream>
+using namespace std;
+int main() {
+    int array[5]={10,20,30, 40 , 50};
+    cout<<array[2]<<endl;
+   // cout<<array;
+
+    return 0;
+}
---
+++ Array2.cpp
@@ -0,0 +1,15 @@
+//Writ a array program that take array size from user
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i < n ; i++){
+        cout<<arr[i]<<" ";
+    }
+}
